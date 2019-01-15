85d71b4275f1a009c3b5ec33062eddf91b26a8c4
tuanhuynh15 <45989371+tuanhuynh15@users.noreply.github.com>
2019-01-15 19:44:33 +0700

    v1

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+ 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+ 
+app.listen(port, function () {
+  console.log("Server listening on port " + port);
+});
